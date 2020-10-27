80afc4363f9b7c37f1954cae8ab807fd07428b69
Mrtly <mrtly.ca@gmail.com>
2020-10-27 15:26:58 -0700

    add data & store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,32 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import jsonData from "@/assets/QsData.json";
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    data: jsonData,
+    gameOn: false, // true when game starts
+    round: 1, // round counter linked to display of current question
+    score: 0
   },
   mutations: {
+    startGame(state) {
+      state.gameOn = true
+    },
+    endGame(state) {
+      state.gameOn = false
+    },
+    increment(state) {
+      state.round++
+    },
+    scoreUp(state) {
+      state.score++
+    }
   },
-  actions: {
+  getters: {
+    round: state => {
+      return state.round;
+    }
   },
-  modules: {
-  }
 })
